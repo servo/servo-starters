e4af7357fa9cecbe811843f0a782eda14995af58
Sam Gibson <sam@ifdown.net>
2015-09-18 08:59:17 +1200

    Add potentially open issues list

--- site.js
+++ site.js
@@ -1,6 +1,35 @@
 var d = React.DOM;
 
-var getIssues = function(callback) {
+var getPotentiallyOpenIssues = function(callback) {
+    var issuesUrl = "https://api.github.com/search/issues";
+    var today = new Date(),
+        twoWeeksAgo = new Date(today - 86400000 * 14),
+        olderThanTwoWeeks = "<" + twoWeeksAgo.toISOString().slice(0, 10);
+
+    var easy = $.ajax({
+        dataType: "json",
+        url: issuesUrl,
+        data: "q=created:" + olderThanTwoWeeks + "+state:open+label:C-assigned+label:E-Easy+repo:servo/servo&sort=created"
+    });
+
+    var lessEasy = $.ajax({
+        dataType: "json",
+        url: issuesUrl,
+        data: "q=created:" + olderThanTwoWeeks + "+state:open+label:C-assigned+label:\"E-Less%20easy\"+repo:servo/servo&sort=created"
+    });
+
+    $.when(easy, lessEasy).done(function(r1, r2) {
+        var easies = r1[0].items,
+            lessEasies = r2[0].items,
+            all = easies.concat(lessEasies);
+
+        all.sort(function(l, r) { return r.created_at.localeCompare(l.created_at) });
+
+        callback(all);
+    });
+};
+
+var getOpenIssues = function(callback) {
     var issuesUrl = "https://api.github.com/search/issues";
 
     var easy = $.ajax({
@@ -39,7 +68,7 @@ var label = function(data) {
 
 var Labels = React.createClass({
     render: function() {
-        return d.span(
+        return d.div(
             {className: "labels"},
             this.props.labels.map(label)
         );
@@ -54,19 +83,22 @@ var Issue = React.createClass({
     render: function() {
         return d.li(
             {className: "issue"},
-            "[ ",
-            d.a(
-                {
-                    className: "issue-link",
-                    href: this.props.html_url,
-                    title: this.props.title
-                },
-                this.props.number
-            ),
-            " ] - ",
-            d.span(
-                {className: "issue-desc"},
-                this.props.title
+            d.div(
+                {},
+                "[ ",
+                d.a(
+                    {
+                        className: "issue-link",
+                        href: this.props.html_url,
+                        title: this.props.title
+                    },
+                    this.props.number
+                ),
+                " ] - ",
+                d.span(
+                    {className: "issue-desc"},
+                    this.props.title
+                )
             ),
             labels(this.props)
         );
@@ -79,7 +111,7 @@ var issueItem = function(data) {
 
 var IssueList = React.createClass({
     componentDidMount: function() {
-        getIssues(function(data) {
+        this.props.loader(function(data) {
             if (this.isMounted()) {
                 this.setState({
                     issues: data,
@@ -109,7 +141,12 @@ var IssueList = React.createClass({
 });
 
 React.render(
-    React.createElement(IssueList, {}),
+    React.createElement(IssueList, {loader: getOpenIssues}),
     document.getElementById("open-issues")
+);
+
+React.render(
+    React.createElement(IssueList, {loader: getPotentiallyOpenIssues}),
+    document.getElementById("potentially-open-issues")
 );
 
